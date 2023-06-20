f758d264593c39c1ec662fbbb50d6b1c9e745ae1
ElenaZu <128751514+ElenaZu@users.noreply.github.com>
2023-06-20 02:02:25 +0000

    Ready

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['jogger','racoon'];
+var domain = ['.com','.net','.us','.io'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for(let i = 0; i < pronoun.length; i++){
+  for(let j = 0; j < adj.length; j++){
+    for(let k = 0; k < noun.length; k++){
+      for(let l =0; l < domain.length;l++){
+        console.log(pronoun[i]+adj[j]+noun[k]+domain[l]);
+      }
+      
+    }
+  }
+}
